3ca9f1810ab792c3b5d2941f103829511d4b4c0d
scorpion-29 <htiholov@gmail.com>
2024-08-04 21:33:35 -0400

    first commit

---
+++ src/auth.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./lib/prisma";
+import Google from "next-auth/providers/google";
+import { Adapter } from "next-auth/adapters";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
+  theme: {
+    logo: "/logo.png",
+  },
+  adapter: PrismaAdapter(prisma) as Adapter,
+  callbacks: {
+    session({ session, user }) {
+      session.user.role = user.role;
+      return session;
+    },
+  },
+  providers: [Google],
+});
